f95e1f63142615b7c2c8de4145e1d9a970151874
realwall <realwall@126.com>
2011-12-11 23:41:02 +0800

    Modified a.c to print the parameter.

--- a.c
+++ a.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 int main(int argc, char **argv)
 {
+	int i;
 	printf("test git.\n");
+	printf("parameter count:%d\n", argc - 1);
+	for(i = 0; i < argc; i++)
+	{
+		printf("parameter[%d]:%s\n", i, argv[i]);
+	}
 	return 0;
 }
